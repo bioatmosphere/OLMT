1f8f1bd1c611d4cddf7e6df8d3d4db64c56b5a7e
bioatmosphere <wbwenwu@gmail.com>
2025-05-28 20:19:10 -0700

    corrected Perlmutter time format

--- model_ELM/ensemble.py
+++ model_ELM/ensemble.py
@@ -47,7 +47,7 @@ def create_samples(self,sampletype='monte_carlo',nsamples=100,parm_list=''):
     os.system('mkdir -p parm_samples')
     np.savetxt(self.ensemble_file,np.transpose(self.samples))
 
-def create_ensemble_script(self, walltime=24):
+def create_ensemble_script(self, walltime=6):
     """Create the PBS script we will submit to run the ensemble
 
     """
@@ -67,7 +67,7 @@ def create_ensemble_script(self, walltime=24):
     if (self.queue == 'debug'):
         walltime=2
     if ('pm-cpu' in self.machine):
-        myfile.write('#SBATCH -t '+str(walltime)+'\n')
+        myfile.write('#SBATCH --time='+str(walltime)+':00:00\n')
         myfile.write('#SBATCH --constraint=cpu\n')
     else:
         myfile.write('#SBATCH -t '+str(walltime)+':00:00\n')
@@ -75,7 +75,7 @@ def create_ensemble_script(self, walltime=24):
     myfile.write('#SBATCH --nodes='+str(nnodes)+'\n')  
     if (self.project != ''):
         myfile.write('#SBATCH -A '+self.project+'\n')
-    myfile.write('#SBATCH -p '+self.queue+'\n')
+    myfile.write('#SBATCH -q '+self.queue+'\n')
     myfile.write('cd '+self.caseroot+'/'+self.casename+'\n')
     myfile.write('export LD_LIBRARY_PATH='+ldpath+'\n\n')
     myfile.write('./preview_namelists\n\n')
@@ -122,7 +122,7 @@ def create_multisite_script(self,sites,scriptdir, walltime=6):
     if (self.queue == 'debug'):
         walltime=2
     if ('pm-cpu' in self.machine):
-        myfile.write('#SBATCH -t '+str(walltime)+'\n')
+        myfile.write('#SBATCH -time='+str(walltime)+':00:00\n')
         myfile.write('#SBATCH --constraint=cpu\n')
     else:
         myfile.write('#SBATCH -t '+str(walltime)+':00:00\n')
