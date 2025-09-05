4950b76a42ed8891a073994c6a2be44cf21df44d
Bin Wang <wbwenwu@gmail.com>
2025-09-05 13:27:11 -0400

    corrected pm-cpu time format

--- model_ELM/ensemble.py
+++ model_ELM/ensemble.py
@@ -122,7 +122,7 @@ def create_multisite_script(self,sites,scriptdir, walltime=6):
     if (self.queue == 'debug'):
         walltime=2
     if ('pm-cpu' in self.machine):
-        myfile.write('#SBATCH -time='+str(walltime)+':00:00\n')
+        myfile.write('#SBATCH --time='+str(walltime)+':00:00\n')
         myfile.write('#SBATCH --constraint=cpu\n')
     else:
         myfile.write('#SBATCH -t '+str(walltime)+':00:00\n')
